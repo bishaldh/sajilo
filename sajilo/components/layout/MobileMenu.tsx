a59bb0fc3805ca23f78a45780bb239c3c1d633fe
bishaldh <mirebhai@gmail.com>
2025-08-09 19:21:06 +0545

    Mobile version optimized

--- sajilo/components/layout/MobileMenu.tsx
+++ sajilo/components/layout/MobileMenu.tsx
@@ -14,7 +14,7 @@ export default function MobileMenu({ isMobileMenu, handleMobileMenu }: any) {
 			<div className={`mobile-header-active mobile-header-wrapper-style perfect-scrollbar button-bg-2 ${isMobileMenu ? 'sidebar-visible' : ''}`}>
 				<PerfectScrollbar className="mobile-header-wrapper-inner">
 					<div className="mobile-header-logo">
-						<Link className="d-flex" href="/"><img className="light-mode" alt="Carento" src="/assets/imgs/template/logo-d.svg" /><img className="dark-mode" alt="Carento" src="/assets/imgs/template/logo-w.svg" /></Link>
+						<Link className="d-flex" href="/"><img className="light-mode" alt="Sajilo" src="/assets/imgs/template/logo-d.png" /><img className="dark-mode" alt="Carento" src="/assets/imgs/template/logo-w.png" /></Link>
 						<div className="burger-icon burger-icon-white" onClick={handleMobileMenu} />
 					</div>
 					<div className="mobile-header-content-area">
@@ -24,67 +24,42 @@ export default function MobileMenu({ isMobileMenu, handleMobileMenu }: any) {
 									<ul className="mobile-menu font-heading">
 										<li className={`${isAccordion === 1 ? "active" : ""}`}>
 											<span className="menu-expand" onClick={() => handleAccordion(1)}>
-												<i className="arrow-small-down"></i>
+												
 											</span>
-											<Link href="/index-2">Home Pages</Link>
+											<Link href="/index-2">Home</Link>
 										</li>
 										<li className={` ${isAccordion === 2 ? "active" : ""}`}>
 											<span className="menu-expand" onClick={() => handleAccordion(2)}>
-												<i className="arrow-small-down"></i>
+												
 											</span>
-											<Link href="/cars-list-1">Vehicles</Link>
-											
+											<Link href="/about-us">About Us</Link>
 										</li>
 										<li className={`${isAccordion === 3 ? "active" : ""}`}>
 											<span className="menu-expand" onClick={() => handleAccordion(3)}>
-												<i className="arrow-small-down"></i>
+											
 											</span>
-											<Link href="#">Dealers</Link>
-											<ul className="sub-menu" style={{ display: `${isAccordion == 3 ? "block" : "none"}` }}>
-												<li><Link href="/dealer-listing">Dealers Listing</Link></li>
-												<li><Link href="/dealer-details">Dealer Details</Link></li>
-											</ul>
+											<Link href="/vehicles">Vehicles</Link>
 										</li>
 										<li className={`${isAccordion === 4 ? "active" : ""}`}>
 											<span className="menu-expand" onClick={() => handleAccordion(4)}>
-												<i className="arrow-small-down"></i>
+												
 											</span>
-											<Link href="#">Shop</Link>
+											<Link href="/dealers">Dealers</Link>
 											<ul className="sub-menu" style={{ display: `${isAccordion == 4 ? "block" : "none"}` }}>
-												<li><Link href="/shop-list">Shop Grid</Link></li>
-												<li><Link href="/shop-details">Product Details</Link></li>
 											</ul>
 										</li>
 										<li className={`${isAccordion === 5 ? "active" : ""}`}>
 											<span className="menu-expand" onClick={() => handleAccordion(5)}>
-												<i className="arrow-small-down"></i>
+												
 											</span>
-											<Link href="/about-us">Pages</Link>
-											<ul className="sub-menu" style={{ display: `${isAccordion == 5 ? "block" : "none"}` }}>
-												<li><Link href="/about-us">About Us</Link></li>
-												<li><Link href="/services">Our Services</Link></li>
-												<li><Link href="/pricing">Pricing</Link></li>
-												<li><Link href="/calculator">Loan Calculator</Link></li>
-												<li><Link href="/faqs">FAQs</Link></li>
-												<li><Link href="/term">Term</Link></li>
-												<li><Link href="/contact">Contact</Link></li>
-												<li><Link href="/login">Login</Link></li>
-												<li><Link href="/register">Register</Link></li>
-												<li><Link href="/404">Error 404</Link></li>
-											</ul>
+											<Link href="/services">Services</Link>
 										</li>
-										<li className={`has-children ${isAccordion === 6 ? "active" : ""}`}>
+										<li className={`${isAccordion === 6 ? "active" : ""}`}>
 											<span className="menu-expand" onClick={() => handleAccordion(6)}>
-												<i className="arrow-small-down"></i>
+												
 											</span>
-											<Link href="#">News</Link>
-											<ul className="sub-menu" style={{ display: `${isAccordion == 6 ? "block" : "none"}` }}>
-												<li><Link href="/blog-grid">News Grid</Link></li>
-												<li><Link href="/blog-list">News List</Link></li>
-												<li><Link href="/blog-details">News Details</Link></li>
-											</ul>
+											<Link href="/contact">Contact</Link>
 										</li>
-										<li><Link href="/contact">Contact</Link></li>
 									</ul>
 								</nav>
 							</div>
